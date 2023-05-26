fa297b19d58cd5501231127b830f72232f3c0f24
David Tega <davidogbodu3056@gmail.com>
2023-05-26 17:10:15 +0100

    First Commit

---
+++ src/main/java/com/neptunesoftware/Accelerex/AccelerexApplication.java
@@ -0,0 +1,13 @@
+package com.neptunesoftware.Accelerex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccelerexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccelerexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neptunesoftware/Accelerex/AccelerexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neptunesoftware.Accelerex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccelerexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
