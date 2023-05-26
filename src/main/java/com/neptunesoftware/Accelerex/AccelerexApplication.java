d32113b86754f8b632f69e11155f3ff786e38358
David Tega <davidogbodu3056@gmail.com>
2023-05-26 18:01:08 +0100

    New Commit

--- src/main/java/com/neptunesoftware/Accelerex/AccelerexApplication.java
+++ src/main/java/com/neptunesoftware/Accelerex/AccelerexApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AccelerexApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AccelerexApplication.class, args);
 	}
-
 }
---
+++ src/main/java/com/neptunesoftware/Accelerex/transaction/TransactionController.java
@@ -0,0 +1,9 @@
+package com.neptunesoftware.Accelerex.transaction;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/transactions")
+public class TransactionController {
+}
